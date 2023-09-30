20466b1e53a3207f3435ffb371a296dae3b900ff
William Nascimento <williamnnmiranda@gmail.com>
2023-09-29 21:26:24 -0300

    feat: :sparkles: created css base from the software section

---
+++ src/componentes/software/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import * as C from "./styles"
+
+const Software = () => {
+  return <div>index</div>;
+};
+
+export default Software;
---
+++ src/componentes/software/styles.tsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const ContainerSoftware = styled.section``;
+
+export const TittleSoftware = styled.h3``;
+
+export const SubTittle = styled.p``;
+
+export const ContainerCards = styled.section``;
+
+export const Card = styled.article``;
+
+export const ContainerIconCard = styled.div``;
+
+export const IconCard = styled.img``;
+
+export const TittleCard = styled.h3``;
+
+export const TextCard = styled.p``;
